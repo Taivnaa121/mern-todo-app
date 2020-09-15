bee7827810b5146fee9e68d55e344b8da275eddf
Taivanjargal <uulaa2@gmail.com>
2020-09-16 01:08:22 +0800

    Updates

---
+++ precache-manifest.74cad1ba2341135614221580d83b9e51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e3a9912a16d0f0a5d89b8c49f0efdc",
+    "url": "/Taivnaa121/mern-todo-app/index.html"
+  },
+  {
+    "revision": "5faa6d7fbc1ced41f684",
+    "url": "/Taivnaa121/mern-todo-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a751988f0891517b29f5",
+    "url": "/Taivnaa121/mern-todo-app/static/css/main.89ad99c1.chunk.css"
+  },
+  {
+    "revision": "5faa6d7fbc1ced41f684",
+    "url": "/Taivnaa121/mern-todo-app/static/js/2.11e4094f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Taivnaa121/mern-todo-app/static/js/2.11e4094f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a751988f0891517b29f5",
+    "url": "/Taivnaa121/mern-todo-app/static/js/main.51be119b.chunk.js"
+  },
+  {
+    "revision": "55ba6461a9f9c700ea6a",
+    "url": "/Taivnaa121/mern-todo-app/static/js/runtime-main.acba77d8.js"
+  }
+]);
